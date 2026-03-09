3a9e6af3a4ecd0520e83e49bfe7044f90d77c369
Charlie Whittleman <brennwh@siue.edu>
2026-03-09 08:51:46 -0500

    Update again

--- nvim/.config/nvim/lua/plugins/nixd.lua
+++ nvim/.config/nvim/lua/plugins/nixd.lua
@@ -6,8 +6,16 @@ require("lspconfig").nixd.setup({
         expr = "import <nixpkgs> { }",
       },
       formatting = {
-        command = { "nixfmt" },
+        command = { "nixfmt" }, -- or nixfmt or nixpkgs-fmt
       },
+      -- options = {
+      --   nixos = {
+      --       expr = '(builtins.getFlake "/PATH/TO/FLAKE").nixosConfigurations.CONFIGNAME.options',
+      --   },
+      --   home_manager = {
+      --       expr = '(builtins.getFlake "/PATH/TO/FLAKE").homeConfigurations.CONFIGNAME.options',
+      --   },
+      -- },
     },
   },
 })
